02ec525cea9aa405c6587fcfec4bd3e27c5b6129
Daniel Ras <ras98.daniel@gmail.com>
2020-08-09 13:39:37 +0300

    feat: add WebParserUtils static class

---
+++ MoldovaExchangeRateProcessor.WebParser/Utils/WebParserUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoldovaExchangeRateProcessor.WebParser.Utils
+{
+    public static class WebParserUtils
+    {
+        public static string Substr (string source, string fromOccurrence, string toOccurrence)
+        {
+            int startIndex, endIndex, contentLength;
+
+            startIndex = source.IndexOf(fromOccurrence);
+            endIndex = source.IndexOf(toOccurrence);
+
+            if (startIndex == -1 || endIndex == -1)
+                throw new Exception("Invalid source Exception");
+
+            contentLength = endIndex - startIndex;
+
+            return source.Substring(startIndex, contentLength);
+        }
+    }
+}
