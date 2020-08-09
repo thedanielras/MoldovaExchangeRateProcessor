9008a6c4b3f711f3d7c932dc4ca34331c8facb9a
Daniel Ras <ras98.daniel@gmail.com>
2020-08-09 13:42:49 +0300

    feat: extract WebWorker's interface for flexibility of realisation

---
+++ MoldovaExchangeRateProcessor.WebParser/Models/IWebWorker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoldovaExchangeRateProcessor.WebParser.Models
+{
+    public interface IWebWorker
+    {
+        public string GetHtml(string url);
+    }
+}
--- MoldovaExchangeRateProcessor.WebParser/WebWorker.cs
+++ MoldovaExchangeRateProcessor.WebParser/WebWorker.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using MoldovaExchangeRateProcessor.WebParser.Models;
+using System;
 using System.Net;
 using System.Net.Http;
 
 namespace MoldovaExchangeRateProcessor.WebParser
 {
-    public class WebWorker
+    public class WebWorker : IWebWorker
     {
         private HttpClient httpClient;
 
@@ -13,10 +14,12 @@ namespace MoldovaExchangeRateProcessor.WebParser
             httpClient = new HttpClient();
         }
 
-        public string GetHtml(string url) {
+        public string GetHtml(string url)
+        {
             string responseBody = httpClient.GetStringAsync(url).GetAwaiter().GetResult();
+            string htmlContent = Utils.WebParserUtils.Substr(responseBody, "<html", "</html");
 
-            return responseBody;
+            return htmlContent;
         }
     }
 }
