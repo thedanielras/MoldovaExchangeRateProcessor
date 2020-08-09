3583e8b4e7a83ed6a6b63cf85fa85b12727c1ef1
Daniel Ras <ras98.daniel@gmail.com>
2020-08-09 13:40:31 +0300

    feat: add Bank model

---
+++ MoldovaExchangeRateProcessor.WebParser/Models/Bank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoldovaExchangeRateProcessor.WebParser.Models
+{
+    public abstract class Bank
+    {
+        public string Name { get; private set; }
+        public string WebUrl { get; private set; }
+
+        protected string webHtmlContent { get; set; }
+        protected List<ExchangeRate> exchangeRates;
+        private IWebWorker webWorker { get; set; }
+
+        public Bank(string name, string webUrl, IWebWorker webWorker)
+        {
+            Name = name;
+            WebUrl = webUrl;
+            this.webWorker = webWorker;
+
+            setWebContent();
+        }               
+        private void setWebContent()
+        {
+            string html = webWorker.GetHtml(WebUrl);
+
+            webHtmlContent = html;
+        }
+        public abstract List<ExchangeRate> GetExchangeRates();
+    }
+}
