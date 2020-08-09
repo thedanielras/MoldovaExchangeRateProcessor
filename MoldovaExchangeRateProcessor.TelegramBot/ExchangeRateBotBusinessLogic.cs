d5b4284a4fbd359c05efef562dfa206fa557c111
Daniel Ras <ras98.daniel@gmail.com>
2020-08-09 22:42:20 +0300

    feat: incapsulate bot's business logic

---
+++ MoldovaExchangeRateProcessor.TelegramBot/ExchangeRateBotBusinessLogic.cs
@@ -0,0 +1,29 @@
+﻿using MoldovaExchangeRateProcessor.TelegramBot.Commands;
+using MoldovaExchangeRateProcessor.WebParser.Models;
+using MoldovaExchangeRateProcessor.WebParser.Models.Banks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoldovaExchangeRateProcessor.TelegramBot
+{
+    class ExchangeRateBotBusinessLogic : IBotBusinessLogic
+    {
+        public string RunCommand(string commandLiteral) => GetCommand(commandLiteral).Execute();
+
+        private ICommand GetCommand(string commandLiteral)
+        {
+            switch (commandLiteral)
+            {
+                case "start":
+                    return new HelpCommand();
+                case "help":
+                    return new HelpCommand();
+                case "getExchange":
+                    return new GetExchangeRateCommand(new List<Bank>() { new MoldovaAgroindbank() });
+                default:
+                    return new UnknownCommand();
+            }
+        }
+    }
+}
---
+++ MoldovaExchangeRateProcessor.TelegramBot/IBotBusinessLogic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoldovaExchangeRateProcessor.TelegramBot
+{
+    interface IBotBusinessLogic
+    {
+        public string RunCommand(string commandLiteral);
+    }
+}
