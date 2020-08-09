728e99d113011c39bc6f56f0e2e27798c2da7407
Daniel Ras <ras98.daniel@gmail.com>
2020-08-09 13:40:16 +0300

    feat: add ExchangeRate model

---
+++ MoldovaExchangeRateProcessor.WebParser/Models/ExchangeRate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoldovaExchangeRateProcessor.WebParser.Models
+{
+    public enum ExchangeRateCurrency
+    {
+        EUR,
+        USD,
+        RON
+    }
+
+    public class ExchangeRate
+    {
+        public double BuyRate { get; set; }
+        public double SellRate { get; set; }
+        public ExchangeRateCurrency Currency { get; private set; }
+
+        public ExchangeRate(double buyRate, double sellRate, ExchangeRateCurrency currency)
+        {
+            BuyRate = buyRate;
+            SellRate = sellRate;
+            Currency = currency;
+        }
+    }
+}
