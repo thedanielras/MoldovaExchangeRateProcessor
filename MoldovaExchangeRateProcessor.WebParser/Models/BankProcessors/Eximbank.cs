8a1382a511330ef87b5c888b4bb86defa0b7faf6
Daniel Ras <ras98.daniel@gmail.com>
2020-08-26 23:21:16 +0300

    feature: add "Eximbank" bank processor

---
+++ MoldovaExchangeRateProcessor.WebParser/Models/BankProcessors/Eximbank.cs
@@ -0,0 +1,51 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoldovaExchangeRateProcessor.WebParser.Models.BankProcessors
+{
+    public class Eximbank : BankProcessor
+    {
+        public Eximbank() : base("Eximbank", "https://eximbank.md/ro/exchange", WebWorker.GetInstance())
+        {
+
+        }
+
+        public async override Task<List<ExchangeRate>> GetExchangeRatesAsync()
+        {
+            List<ExchangeRate> rates = new List<ExchangeRate>();
+
+            var context = BrowsingContext.New(Configuration.Default);
+
+            //Create a document from a virtual request / response pattern
+            var document = await context.OpenAsync(req => req.Content(webHtmlContent));
+
+            var usdRate = GetExchangeRateByWebSiteTableColumn(document, 2, ExchangeRateCurrency.USD);
+            rates.Add(usdRate);
+            var eurRate = GetExchangeRateByWebSiteTableColumn(document, 3, ExchangeRateCurrency.EUR);
+            rates.Add(eurRate);
+            var rubRate = GetExchangeRateByWebSiteTableColumn(document, 4, ExchangeRateCurrency.RUB);
+            rates.Add(rubRate);
+            var ronRate = GetExchangeRateByWebSiteTableColumn(document, 5, ExchangeRateCurrency.RON);
+            rates.Add(ronRate);
+
+            return rates;
+        }
+
+        private ExchangeRate GetExchangeRateByWebSiteTableColumn(IDocument document, int numComlumn, ExchangeRateCurrency currency)
+        {
+            ExchangeRate rate = null;
+
+            string buyRateQuery = $"section.exchange-rates table tbody tr:nth-child({numComlumn}) td:nth-child(2)";
+            string sellRateQuery = $"section.exchange-rates table tbody tr:nth-child({numComlumn}) td:nth-child(3)";
+
+            rate =  GetExchangeRateFromDocumentByQuery(document, buyRateQuery, sellRateQuery, currency);            
+
+            return rate;
+        }
+    }
+}
