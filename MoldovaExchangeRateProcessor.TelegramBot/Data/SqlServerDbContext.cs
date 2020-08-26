2175085fba2b2381d78dec7cbb6abc474b44995c
Daniel Ras <ras98.daniel@gmail.com>
2020-08-26 23:07:37 +0300

    feat: add Data Access mechanisms for db integration

---
+++ MoldovaExchangeRateProcessor.TelegramBot/Data/SqlServerDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoldovaExchangeRateProcessor.WebParser.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoldovaExchangeRateProcessor.TelegramBot.Data
+{
+    class SqlServerDbContext : DbContext
+    {
+        public SqlServerDbContext(DbContextOptions<SqlServerDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<ExchangeRate> ExchangeRates { get; set; }
+        public DbSet<Bank> Banks { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                 .Entity<ExchangeRate>()
+                 .Property(e => e.Currency)
+                 .HasConversion(
+                     v => v.ToString(),
+                     v => (ExchangeRateCurrency)Enum.Parse(typeof(ExchangeRateCurrency), v));
+
+            modelBuilder.Entity<ExchangeRate>()
+                 .HasIndex(e => new { e.BankId, e.Date, e.Currency })
+                 .IsUnique();
+
+            modelBuilder.Entity<Bank>()
+                .HasIndex(u => u.Name)
+                .IsUnique();
+        }
+    }
+}
