4e7796e787bf5959da31f62ad0de5893c95148e9
Daniel Ras <ras98.daniel@gmail.com>
2020-08-26 23:14:46 +0300

    feat: add logger depedency and bot arguments

--- MoldovaExchangeRateProcessor.TelegramBot/ExchangeRateBotBusinessLogic.cs
+++ MoldovaExchangeRateProcessor.TelegramBot/ExchangeRateBotBusinessLogic.cs
@@ -1,27 +1,44 @@
-﻿using MoldovaExchangeRateProcessor.TelegramBot.Commands;
+﻿using Microsoft.Extensions.Logging;
+using MoldovaExchangeRateProcessor.TelegramBot.Commands;
 using MoldovaExchangeRateProcessor.WebParser.Models;
-using MoldovaExchangeRateProcessor.WebParser.Models.Banks;
+using MoldovaExchangeRateProcessor.WebParser.Models.BankProcessors;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MoldovaExchangeRateProcessor.TelegramBot
 {
     class ExchangeRateBotBusinessLogic : IBotBusinessLogic
     {
-        public string RunCommand(string commandLiteral) => GetCommand(commandLiteral).Execute();
+        private readonly IBotClientArgs _botArgs;
+        private readonly ILogger _logger;
+
+        public Task<string> RunCommandAsync(string commandLiteral)
+        {
+            var command = GetCommand(commandLiteral);
+            var result = command.ExecuteAsync();
+            
+            return result;
+        }
+
+        public ExchangeRateBotBusinessLogic(IBotClientArgs botArgs)
+        {
+            _botArgs = botArgs;
+            _logger = botArgs.Logger;
+        }
 
         private ICommand GetCommand(string commandLiteral)
-        {
+        {          
             switch (commandLiteral)
             {
-                case "start":
+                case "start":                    
                     return new HelpCommand();
-                case "help":
-                    return new HelpCommand();
-                case "getExchange":
-                    return new GetExchangeRateCommand(new List<Bank>() { new MoldovaAgroindbank(), new Victoriabank() });
-                default:
+                case "help":                 
+                    return new HelpCommand();              
+                case "getCurrent":                 
+                    return new GetCurrentCommand(_botArgs);       
+                default:                 
                     return new UnknownCommand();
             }
         }
