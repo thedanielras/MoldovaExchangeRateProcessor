d8aa541068cb8f5ebb859c936a3038c52c2e7e35
Daniel Ras <ras98.daniel@gmail.com>
2020-08-28 10:22:04 +0300

    feat: add IsCorrect property, checking for 0 rate

--- MoldovaExchangeRateProcessor.WebParser/Models/ExchangeRate.cs
+++ MoldovaExchangeRateProcessor.WebParser/Models/ExchangeRate.cs
@@ -27,15 +27,23 @@ namespace MoldovaExchangeRateProcessor.WebParser.Models
         public ExchangeRateCurrency Currency { get; private set; }
         [Required]
         public Bank Bank { get; set; }
-        public int  BankId { get; set; }
+        public int BankId { get; set; }
+        public bool IsCorrect
+        {
+            get
+            {
+                if (BuyRate == 0d || SellRate == 0d) return false;
+                return true;
+            }
+        }
 
         public ExchangeRate(double buyRate, double sellRate, ExchangeRateCurrency currency)
-        {   
+        {
             BuyRate = buyRate;
             SellRate = sellRate;
             Currency = currency;
             Date = DateTime.Now;
-        }      
+        }
 
         public override string ToString()
         {
@@ -43,6 +51,6 @@ namespace MoldovaExchangeRateProcessor.WebParser.Models
             nfi.NumberDecimalSeparator = ",";
 
             return $"{Currency.ToString()}: Buy: {BuyRate.ToString("00.00", nfi)} | Sell: {SellRate.ToString("00.00", nfi)}";
-        }
+        }        
     }
 }
